ce2ec50a6e7e4cddd340c2d85977d5d589f0fcd9
Dmitri Pavlenkov <dmitripavlenkov@gmail.com>
2017-04-20 13:45:43 -0400

    fix nuspec

--- src/Actor/Properties/AssemblyInfo.cs
+++ src/Actor/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Dagmatic Akka Actors")]
-[assembly: AssemblyDescription("Workflows for Akka.Net")]
+[assembly: AssemblyTitle("Dagmatic workflows for akka.net")]
+[assembly: AssemblyDescription("Dagmatic Akka")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Dagmatic")]
-[assembly: AssemblyProduct("Dagmatic.Akka.Actors")]
+[assembly: AssemblyProduct("Dagmatic.Akka.Actor")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
